35576ed21180b8139fee0f360a51005c773bfdf3
Chamly <vladskotarenko6227@gmail.com>
2023-03-08 12:23:26 +0200

    add game skeleton

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,36 @@
-#include <iostream>
+#include "iostream"
+#include "SFML/Graphics.hpp"
+#include "SFML/System.hpp"
+#include "SFML/Window.hpp"
 
-using namespace std;
+int main()
+{
 
-int main() {
-    cout << "Hello, World!" << endl;
+    // Window setup
+    sf::RenderWindow window(sf::VideoMode(800, 600), "test_game", sf::Style::Default);
+    sf::Event ev;
+
+    // Game loop
+    while (window.isOpen()) {
+        // Event polling
+        while (window.pollEvent(ev)) {
+            switch (ev.type) {
+                case sf::Event::Closed:
+                    window.close();
+                    break;
+                case sf::Event::KeyPressed:
+//                    std::cout << "key pressed" << std::endl;
+//                    std::cout << ev.key.code << std::endl;
+                    break;
+            }
+        }
+
+        // Update
+
+        // Render
+        window.clear();
+        window.display();
+    }
+
     return 0;
 }
