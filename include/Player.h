55bc05cd4ed9567192c3c7c39958c4b957804565
Chamly <vladskotarenko6227@gmail.com>
2023-04-14 22:17:19 +0300

    update player input handling so that a specific key can be bound to a specific player action

--- include/Player.h
+++ include/Player.h
@@ -1,16 +1,35 @@
 #ifndef TEST_GAME_PLAYER_H
 #define TEST_GAME_PLAYER_H
 
+#include <map>
+
 #include "SFML/Window/Event.hpp"
 
 #include "CommandQueue.h"
 
+enum class PlayerAction {
+    MoveUp,
+    MoveDown,
+    MoveLeft,
+    MoveRight
+};
+
 class Player {
 private:
+    std::map<sf::Keyboard::Key, PlayerAction> keyBinding;
+    std::map<PlayerAction, Command> playerActionBinding;
+
+    bool isRealtimePlayerAction(PlayerAction action);
+
+    void initialKeyBinding();
+    void initialPlayerActionBinding();
 
 public:
+    Player();
     void handleEvent(const sf::Event& event, CommandQueue& commands);
     void handleRealtimeInput(CommandQueue& commands);
+
+    void assignKey(PlayerAction action, sf::Keyboard::Key key);
 };
 
 #endif //TEST_GAME_PLAYER_H
--- src/Player.cpp
+++ src/Player.cpp
@@ -2,15 +2,50 @@
 #include "SceneNode.h"
 #include "Entities/Unit.h"
 
+bool Player::isRealtimePlayerAction(PlayerAction action) {
+    switch (action) {
+        case PlayerAction::MoveUp:
+        case PlayerAction::MoveDown:
+        case PlayerAction::MoveLeft:
+        case PlayerAction::MoveRight:
+            return true;
+
+        default:
+            return false;
+    }
+}
+
+void Player::initialKeyBinding() {
+    keyBinding[sf::Keyboard::Key::Up] = PlayerAction::MoveUp;
+    keyBinding[sf::Keyboard::Key::Down] = PlayerAction::MoveDown;
+    keyBinding[sf::Keyboard::Key::Left] = PlayerAction::MoveLeft;
+    keyBinding[sf::Keyboard::Key::Right] = PlayerAction::MoveRight;
+}
+
+void Player::initialPlayerActionBinding() {
+    playerActionBinding[PlayerAction::MoveUp].action = EntityMover(Direction::Up);
+    playerActionBinding[PlayerAction::MoveDown].action = EntityMover(Direction::Down);
+    playerActionBinding[PlayerAction::MoveLeft].action = EntityMover(Direction::Left);
+    playerActionBinding[PlayerAction::MoveRight].action = EntityMover(Direction::Right);
+
+    for (auto& pair : playerActionBinding) {
+        pair.second.sceneNodeCategory = SceneNodeCategory::Player;
+    }
+}
+
+Player::Player() {
+    initialKeyBinding();
+    initialPlayerActionBinding();
+}
+
 void Player::handleEvent(const sf::Event& event, CommandQueue& commands) {
 
 }
 
 void Player::handleRealtimeInput(CommandQueue& commands) {
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) {
-        Command moveUp;
-        moveUp.sceneNodeCategory = SceneNodeCategory::Player;
-        moveUp.action = EntityMover(Direction::Up);
-        commands.push(moveUp);
+    for (auto pair : keyBinding) {
+        if (sf::Keyboard::isKeyPressed(pair.first) && isRealtimePlayerAction(pair.second)) {
+            commands.push(playerActionBinding[pair.second]);
+        }
     }
 }
