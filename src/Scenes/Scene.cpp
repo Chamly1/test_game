957237d63448f8245bdc6c023e31b8ff5f760f91
Chamly <vladskotarenko6227@gmail.com>
2023-04-17 01:21:20 +0300

    add the Scene class's protected methods definition

--- src/Scenes/Scene.cpp
+++ src/Scenes/Scene.cpp
@@ -1,4 +1,21 @@
 #include "Scenes/Scene.h"
+#include "Scenes/SceneList.h"
+
+void Scene::requestListPushBack(SceneIdentifier sceneId) {
+    sceneList->pushBack(sceneId);
+}
+
+void Scene::requestListPopBack() {
+    sceneList->popBack();
+}
+
+void Scene::requestListClear() {
+    sceneList->clear();
+}
+
+SceneContext Scene::getSceneContext() const {
+    return ctx;
+}
 
 Scene::Scene(SceneContext ctx, SceneList& sceneList)
 : ctx(ctx)
