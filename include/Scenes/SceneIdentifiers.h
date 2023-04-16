3a9dcc64696d3385f636288aac8ddff6d410d578
Chamly <vladskotarenko6227@gmail.com>
2023-04-16 09:18:16 +0300

    add enum for scene identifiers

---
+++ include/Scenes/SceneIdentifiers.h
@@ -0,0 +1,13 @@
+#ifndef TEST_GAME_SCENEIDENTIFIERS_H
+#define TEST_GAME_SCENEIDENTIFIERS_H
+
+enum class SceneIdentifier {
+    None,
+    Title,
+    Menu,
+    Game,
+    Loading,
+    Pause
+};
+
+#endif //TEST_GAME_SCENEIDENTIFIERS_H
