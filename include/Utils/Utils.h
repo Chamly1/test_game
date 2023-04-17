c6263bb42fd162e7955fa5b5ede4e6f4cc2acff2
Chamly <vladskotarenko6227@gmail.com>
2023-04-18 02:59:33 +0300

    add overload setOriginToCenter() for Sprites

--- include/Utils/Utils.h
+++ include/Utils/Utils.h
@@ -2,7 +2,9 @@
 #define TEST_GAME_UTILS_H
 
 #include "SFML/Graphics/Text.hpp"
+#include "SFML/Graphics/Sprite.hpp"
 
 void setOriginToCenter(sf::Text& text);
+void setOriginToCenter(sf::Sprite& sprite);
 
 #endif //TEST_GAME_UTILS_H
--- src/Utils/Utils.cpp
+++ src/Utils/Utils.cpp
@@ -6,3 +6,8 @@ void setOriginToCenter(sf::Text& text) {
     sf::FloatRect bounds = text.getLocalBounds();
     text.setOrigin(std::floor(bounds.left + bounds.width / 2.f), std::floor(bounds.top + bounds.height / 2.f));
 }
+
+void setOriginToCenter(sf::Sprite& sprite) {
+    sf::FloatRect bounds = sprite.getLocalBounds();
+    sprite.setOrigin(std::floor(bounds.left + bounds.width / 2.f), std::floor(bounds.top + bounds.height / 2.f));
+}
