2644d69eb647d536f95cb791974b75cc54834d3d
Chamly <vladskotarenko6227@gmail.com>
2023-04-20 01:20:39 +0300

    add GUI Button

---
+++ include/GUI/Button.h
@@ -0,0 +1,32 @@
+#ifndef TEST_GAME_BUTTON_H
+#define TEST_GAME_BUTTON_H
+
+#include "GUI/Component.h"
+
+#include "SFML/Graphics/Text.hpp"
+#include "SFML/Graphics/Sprite.hpp"
+
+#include <functional>
+
+namespace GUI {
+
+class Button : public Component {
+private:
+    std::function<void()> callback;
+    sf::Text text;
+    sf::Sprite selector;
+
+    virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const;
+
+public:
+    Button(const std::string& text, const sf::Font& font, const sf::Texture& selectorTexture);
+    void setCallback(std::function<void()> callback);
+    virtual bool isSelectable() const;
+    virtual void activate();
+    virtual void handleEvent(const sf::Event& event);
+
+};
+
+}
+
+#endif //TEST_GAME_BUTTON_H
---
+++ src/GUI/Button.cpp
@@ -0,0 +1,40 @@
+#include "GUI/Button.h"
+
+#include "SFML/Graphics/RenderTarget.hpp"
+
+namespace GUI {
+
+void Button::draw(sf::RenderTarget& target, sf::RenderStates states) const {
+    states.transform *= getTransform();
+    target.draw(text, states);
+    if (isActive()) {
+        target.draw(selector, states);
+    }
+}
+
+Button::Button(const std::string& text, const sf::Font& font, const sf::Texture& selectorTexture)
+: callback()
+, text(text, font)
+, selector(selectorTexture) {
+
+}
+
+void Button::setCallback(std::function<void()> callback) {
+    this->callback = std::move(callback);
+}
+
+bool Button::isSelectable() const {
+    return true;
+}
+
+void Button::activate() {
+    if (callback) {
+        callback();
+    }
+}
+
+void handleEvent(const sf::Event& event) {
+
+}
+
+}
