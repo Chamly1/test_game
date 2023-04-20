0579a2b9720f869e36879609511b514c88b42992
Chamly <vladskotarenko6227@gmail.com>
2023-04-21 00:51:50 +0300

    add functional buttons to PauseScene

--- include/Scenes/PauseScene.h
+++ include/Scenes/PauseScene.h
@@ -2,14 +2,20 @@
 #define TEST_GAME_PAUSESCENE_H
 
 #include "Scenes/Scene.h"
+#include "GUI/Container.h"
+#include "GUI/Label.h"
+#include "GUI/ButtonList.h"
 
 #include "SFML/Graphics/Text.hpp"
 #include "SFML/Graphics/RectangleShape.hpp"
 
 class PauseScene : public Scene {
 private:
-    sf::Text title;
+    GUI::Container guiContainer;
     sf::RectangleShape backgroundShape;
+
+    std::shared_ptr<GUI::Label> createTitle(const sf::Font& font);
+    std::shared_ptr<GUI::ButtonList> createButtons(const sf::Font& font, const sf::Texture& selectorTexture);
 
 public:
     PauseScene(SceneContext ctx, SceneList& sceneList);
--- src/Scenes/PauseScene.cpp
+++ src/Scenes/PauseScene.cpp
@@ -2,17 +2,55 @@
 #include "Utils/Utils.h"
 
 const unsigned int titleCharacterSize = 128;
+const unsigned int buttonsCharacterSize = 64;
+
+std::shared_ptr<GUI::Label> PauseScene::createTitle(const sf::Font& font) {
+    std::shared_ptr<GUI::Label> title = std::make_shared<GUI::Label>("Pause", font);
+    title->setPosition(getSceneContext().window->getView().getCenter());
+    title->move(0, title->getPosition().y / -2);
+    title->setCharacterSize(titleCharacterSize);
+
+    return title;
+}
+
+std::shared_ptr<GUI::ButtonList> PauseScene::createButtons(const sf::Font& font, const sf::Texture& selectorTexture) {
+    std::shared_ptr<GUI::ButtonList> buttons = std::make_shared<GUI::ButtonList>(buttonsCharacterSize, font, selectorTexture);
+    buttons->setPosition(getSceneContext().window->getView().getCenter());
+
+    buttons->addButton("resume", [this] () {
+        requestListPopBack();
+    });
+
+    buttons->addButton("settings", [this] () {
+
+    });
+
+    buttons->addButton("quit to main menu", [this] () {
+        requestListClear();
+        requestListPushBack(SceneIdentifier::Menu);
+    });
+
+    buttons->addButton("quit to desktop", [this] () {
+        requestListClear();
+    });
+
+    return buttons;
+}
 
 PauseScene::PauseScene(SceneContext ctx, SceneList& sceneList)
 : Scene(ctx, sceneList)
-, title()
+, guiContainer()
 , backgroundShape() {
-    title.setFont(getSceneContext().fonts->get(FontIdentifier::Main));
-    title.setString("Pause");
-    title.setCharacterSize(titleCharacterSize);
-    setOriginToCenter(title);
-    title.setPosition(getSceneContext().window->getView().getCenter());
-    title.move(0, title.getPosition().y / -2);
+    const sf::Font& mainFont = ctx.fonts->get(FontIdentifier::Main);
+    const sf::Texture& selectorTexture = ctx.textures->get(TextureIdentifier::MenuSelector);
+
+    std::shared_ptr<GUI::Label> title = createTitle(mainFont);
+
+    std::shared_ptr<GUI::ButtonList> buttons = createButtons(mainFont, selectorTexture);
+    buttons->activate();
+
+    guiContainer.pushBack(title);
+    guiContainer.pushBack(buttons);
 
     backgroundShape.setFillColor(sf::Color(0, 0, 0, 150));
     backgroundShape.setSize(ctx.window->getView().getSize());
@@ -22,7 +60,7 @@ void PauseScene::draw() {
     sf::RenderWindow& window = *getSceneContext().window;
 
     window.draw(backgroundShape);
-    window.draw(title);
+    window.draw(guiContainer);
 }
 
 bool PauseScene::update(sf::Time dt) {
@@ -30,15 +68,7 @@ bool PauseScene::update(sf::Time dt) {
 }
 
 bool PauseScene::handleEvent(const sf::Event& event) {
-    if (event.type != sf::Event::KeyPressed) {
-        return false;
-    }
-
-    switch (event.key.code) {
-        case sf::Keyboard::Escape:
-            requestListPopBack();
-            break;
-    }
+    guiContainer.handleEvent(event);
 
     return false;
 }
