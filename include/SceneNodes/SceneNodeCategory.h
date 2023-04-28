79903fa440bdbb66fa5cd6e9f009a797464b61ca
Chamly <vladskotarenko6227@gmail.com>
2023-04-28 22:35:34 +0300

    fix code style in SceneNodeCategory.h

--- include/SceneNodes/SceneNodeCategory.h
+++ include/SceneNodes/SceneNodeCategory.h
@@ -2,14 +2,16 @@
 #define TEST_GAME_SCENENODECATEGORY_H
 
 namespace SceneNodeCategory {
-    enum Type {
-        None = 0,
-        Scene = 1 << 0,
-        PlayerUnit = 1 << 1,
-        EnemyUnit = 1 << 2,
-        Player = 1 << 3,
-        ImpassableZone = 1 << 4
-    };
+
+enum Type {
+    None = 0,
+    Scene = 1 << 0,
+    PlayerUnit = 1 << 1,
+    EnemyUnit = 1 << 2,
+    Player = 1 << 3,
+    ImpassableZone = 1 << 4
+};
+
 }
 
 #endif //TEST_GAME_SCENENODECATEGORY_H
