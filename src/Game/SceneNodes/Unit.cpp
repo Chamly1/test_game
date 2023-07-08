48ffe42e6edcb9b92e1faeb74fdf49cd062bded7
Chamly <vladskotarenko6227@gmail.com>
2023-07-08 06:24:52 +0300

    delete old move Unit collision resolving

--- src/Game/SceneNodes/Unit.cpp
+++ src/Game/SceneNodes/Unit.cpp
@@ -119,55 +119,5 @@ bool Unit::isCollidable() const {
 }
 
 void Unit::onCollision(CollidableNode& collisionWith) {
-    if (collisionWith.getSceneNodeCategory() & SceneNodeCategory::ImpassableZone) {
-        sf::FloatRect collisionBox = getCollisionBoxRect();
-        sf::FloatRect collisionWithBox = collisionWith.getCollisionBoxRect();
-
-        sf::FloatRect intersection;
-        collisionBox.intersects(collisionWithBox, intersection);
-        DebugLog::log("Intersection size: " + std::to_string(intersection.width) + ", " + std::to_string(intersection.height));
-    }
-//    if (collisionWith.getSceneNodeCategory() & SceneNodeCategory::ImpassableZone ||
-//            (collisionWith.getSceneNodeCategory() & SceneNodeCategory::ImpassableUnit && getSceneNodeCategory() & SceneNodeCategory::ImpassableUnit)) {
-//        sf::FloatRect collisionBox = getCollisionBoxRect();
-//        sf::FloatRect collisionWithBox = collisionWith.getCollisionBoxRect();
-//
-//        sf::Vector2f collisionBoxCenter = sf::Vector2f(collisionBox.left + collisionBox.width / 2.f, collisionBox.top + collisionBox.height / 2.f);
-//        // rectangle of the closest points without collision (center if collisionBox must lies on this rectangle side)
-//        sf::FloatRect extendedCollisionWithBox = sf::FloatRect(collisionWithBox.left - collisionBox.width / 2.f,
-//                                                               collisionWithBox.top - collisionBox.height / 2.f,
-//                                                               collisionWithBox.width + collisionBox.width,
-//                                                               collisionWithBox.height + collisionBox.height);
-//
-//        if (extendedCollisionWithBox.contains(collisionBoxCenter)) {
-//            sf::Vector2f rectPos = getRectPosition(extendedCollisionWithBox);
-//            sf::Vector2f rectSize = getRectSize(extendedCollisionWithBox);
-//
-//            sf::Vector2f a = rectPos;
-//            sf::Vector2f b = rectPos; b.x += rectSize.x;
-//            sf::Vector2f c = rectPos + rectSize;
-//            sf::Vector2f d = rectPos; d.y += rectSize.y;
-//
-//            sf::Vector2f reverseMoveDirection = getPreviousVelocity() * -1.f;
-//            sf::Vector2f intersectAt;
-//            sf::Vector2f intersectNormal;
-//            if (isRayIntersectSegment(collisionBoxCenter, reverseMoveDirection, a, b, intersectAt)) {
-//                intersectNormal = unitNormalVector(a, b);
-//            } else if (isRayIntersectSegment(collisionBoxCenter, reverseMoveDirection, b, c, intersectAt)) {
-//                intersectNormal = unitNormalVector(b, c);
-//            } else if (isRayIntersectSegment(collisionBoxCenter, reverseMoveDirection, c, d, intersectAt)) {
-//                intersectNormal = unitNormalVector(c, d);
-//            } else if (isRayIntersectSegment(collisionBoxCenter, reverseMoveDirection, d, a, intersectAt)) {
-//                intersectNormal = unitNormalVector(d, a);
-//            }
-//
-//            sf::Vector2f moveVector = intersectAt - collisionBoxCenter;
-//
-//            // leave only axis which move out from collided object
-//            moveVector.x *= fabsf(intersectNormal.x);
-//            moveVector.y *= fabsf(intersectNormal.y);
-//
-//            move(moveVector);
-//        }
-//    }
+    
 }
